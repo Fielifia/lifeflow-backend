331a204908edd1b22b83194e4b5d273fa5912443
Sofia Andersson <sa226@student.lnu.se>
2026-04-05 13:24:06 +0200

    Add dotenv configuration and create .env.example file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 app.use(cors())
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('LifeFlow backend is running!')
