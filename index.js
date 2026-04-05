5ce1ceffd67131f19c942d0d9480a42163d1336a
Sofia Andersson <sa226@student.lnu.se>
2026-04-05 13:17:34 +0200

    Add CORS middleware and update dependencies in package.json

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 const port = 5000
 
 app.get('/', (req, res) => {
-  res.send('LifeFlow backend is running!')
+    res.send('LifeFlow backend is running!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
